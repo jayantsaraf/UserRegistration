02055268b5b56d4b23d79e8d811eef3f61338b52
jayantsaraf <sarafjayant24@gmail.com>
2020-10-07 08:04:38 +0530

    [jayant]Add. added function to vaidate first name

---
+++ RegExPatterns/Patterns.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegExPatterns
+{
+    public class Patterns
+    {
+        public static string REGEX_FIRST_NAME = "^[A-Z][a-z]{3,}?";
+        public bool ValidateFirstName(string first)
+        {
+            return Regex.IsMatch(first, REGEX_FIRST_NAME);
+        }
+    }
+}
---
+++ RegExPatterns/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace RegExPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration!");
+            Patterns p = new Patterns();
+            RegStart:
+            Console.WriteLine("Enter First Name(should start with capital letter and contain minimum 3 characters");
+            string fName = Console.ReadLine();
+            bool check = p.ValidateFirstName(fName);
+            if(check==false)
+            {
+                Console.WriteLine("Enter Valid First Name");
+                goto RegStart;
+            }
+            else
+            {
+                Console.WriteLine("First Name: " + fName);
+            }
+        }
+    }
+}
